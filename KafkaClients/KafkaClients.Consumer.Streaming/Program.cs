b7a2b687ec7990336f2fa4fbdff7868e100faa5c
Steffen Jørgensen <dk.steffen@gmail.com>
2022-03-31 15:02:00 +0200

    Add Consumer.Regular

--- KafkaClients/KafkaClients.Consumer.Streaming/Program.cs
+++ KafkaClients/KafkaClients.Consumer.Streaming/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
 
 namespace KafkaClients.Consumer.Streaming
 {
@@ -6,7 +9,7 @@ namespace KafkaClients.Consumer.Streaming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
