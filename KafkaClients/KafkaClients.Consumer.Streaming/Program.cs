2588825e923bcf7085b43a4804adf06b4485d77e
Steffen Jørgensen <dk.steffen@gmail.com>
2022-03-31 14:48:19 +0200

    Added Consumer.Streaming

---
+++ KafkaClients/KafkaClients.Consumer.Streaming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KafkaClients.Consumer.Streaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
